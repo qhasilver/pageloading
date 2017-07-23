b31c96da4316485fb716245fe692ff6f9c48feab
qhasilver <qhasilver@qq.com>
2017-07-23 23:15:11 +0800

    Update qhasilver_loading.js

--- qhasilver_loading.js
+++ qhasilver_loading.js
@@ -20,7 +20,7 @@ var qhasilver_t_load = function(img){
 }
 //定义记载中蒙版
 function createLoadingDom(){
-	var html = "<div id='loader' style='position:absolute;top:0px;width:99%;height:1000px;background:white;z-index:9999;'><div style='position:absolute;width:200px;height:120px;border:1px solid gray;border-radius:15px;background:white;margin:0 auto;top:25%;left:0px;right:0px;text-align:center;'><img src='loading.gif' style='margin-top:30px'><div style='margin-top:10px'>加载中...</div><div id='loadProgress' style='margin-top:5px'>0/0</div></div></div>";
+	var html = "<div id='loader' style='position:absolute;top:0px;width:100%;height:1000px;background:white;z-index:9999;'><div style='position:absolute;width:200px;height:120px;border:1px solid gray;border-radius:15px;background:white;margin:0 auto;top:25%;left:0px;right:0px;text-align:center;'><img src='loading.gif' style='margin-top:30px'><div style='margin-top:10px'>加载中...</div><div id='loadProgress' style='margin-top:5px'>0/0</div></div></div>";
 	document.write(html);
 }
 qhasilver_t_load(qhasilver_images[qhasilver_i]);//执行加载中方法
